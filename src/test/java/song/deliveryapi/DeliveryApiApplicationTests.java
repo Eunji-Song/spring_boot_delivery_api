aa20980a2e0ca46c4d60f658cd5e8436d803f0be
song-eunji <eunjisong99@gmail.com>
2023-11-13 02:41:35 +0900

    커밋 안된 프로젝트 파일 커밋

---
+++ src/test/java/song/deliveryapi/DeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package song.deliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
