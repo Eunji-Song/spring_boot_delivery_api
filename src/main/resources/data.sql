a997fc718bcb8ced50cd4b11e2570df69c1a898e
song-eunji <eungjisong99@gmail.com>
2023-11-18 10:42:15 +0900

    애플리케이션 실행시 기본 더미데이터 추가를 위한 sql 파일 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `product` (`cateogry_id`, `created_at`, `description`, `name`, `price`, `store_id`, `updated_at`)
+VALUES
+    (NULL, NULL, 'TEST1 DATA DESCRIPTION', 'TEST DATA 1', 2000.00, NULL, NULL),
+    (NULL, NULL, 'TEST2 DATA DESCRIPTION', 'TEST DATA 2', 5500.00, NULL, NULL),
+    (NULL, NULL, 'TEST3 DATA DESCRIPTION', 'TEST DATA 3', 4300.00, NULL, NULL);
