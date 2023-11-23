1a8d512d6d72bfe4abd15055c2fb71ea7f99c1dc
song-eunji <eungjisong99@gmail.com>
2023-11-24 04:52:09 +0900

    유효성 검사관련 의존성 추가 및 유효성 검사 순서 지정 클래스 생성

---
+++ src/main/java/song/deliveryapi/common/validation/ValidationGroups.java
@@ -0,0 +1,7 @@
+package song.deliveryapi.common.validation;
+
+public class ValidationGroups {
+    public interface NotEmptyGroup{}
+    public interface NotNullGroup{}
+    public interface PatternGroup{}
+}
