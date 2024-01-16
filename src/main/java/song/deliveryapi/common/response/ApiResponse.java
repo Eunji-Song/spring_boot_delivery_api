a017aec24a2f83c47963d80897ddb8d412aaccab
song-eunji <eungjisong99@gmail.com>
2024-01-17 01:32:41 +0900

    feat : 응답 메서드 및 리턴코드 Enum 클래스 값 추가

--- src/main/java/song/deliveryapi/common/response/ApiResponse.java
+++ src/main/java/song/deliveryapi/common/response/ApiResponse.java
@@ -1,117 +1,96 @@
 package song.deliveryapi.common.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static java.lang.Boolean.*;
 
 /**
- * API 공통 응답 포맷 생성
- * 오버로딩을 이용하여 같은 메서드명으로 파라미터만 다르게 할당 받도록 구현
+ * 제네릭 타입을 이용하여 받는 값을 유연하게 처리
  */
+public class ApiResponse {
+    static final ResultCode SUCCESS = ResultCode.SUCCESS;
+    static final ResultCode ERROR = ResultCode.ERROR;
+    static final ResultCode INVALID_PARAMETER = ResultCode.INVALID_PARAMETER;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 
-public class ApiResponse<T> {
-    // == Return 필드 == //
-    private Integer code; // ResultCode.code
-    private String message; // Resultcode.message
-    // null값인 경우 필드 미노출
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private T data; // 리턴 데이터
 
-    // == Response Result Code === //
-    public ApiResponse(ResultCode code) {
-        this.code = code.getCode();
-        this.message = code.getMessage();
-        this.data = data;
+    // == Success == //
+
+    /**
+     * 매개변수를 받지 않는 메서드
+     */
+    public static ApiResult success() {
+        return new ApiResult(SUCCESS);
     }
 
-    public ApiResponse(ResultCode resultCode, String message, T data) {
-        this.code = resultCode.getCode();
-        this.message = resultCode.getMessage();
-        this.data = data;
+    /**
+     * ResultCode를 매개변수로 받는 메서드
+     * 특정 Code에 대한 Response return
+     */
+    public static ApiResult success(ResultCode resultCode) {
+        return new ApiResult<>(resultCode);
     }
 
-    public ApiResponse(ResultCode resultCode, String message) {
-        this.code = resultCode.getCode();
-        this.message = message;
+    /**
+     * Resultcode, Data를 매개변수로 받는 메서드
+     */
+    public static <T> ApiResult<T> success(ResultCode resultCode, T data) {
+        return new ApiResult<>(resultCode, data);
     }
 
-
-    // == Response Custom == //
-    public ApiResponse(Integer code, String message, T data) {
-        this.code = code;
-        this.message = message;
-        this.data = data;
-    }
-
-    public ApiResponse(Integer code, String message) {
-        this.code = code;
-        this.message = message;
+    /**
+     * 성공 리턴시 메시지 내용을 커스텀으로 작성
+     */
+    public static ApiResult success(String message) {
+        return new ApiResult(SUCCESS, message);
     }
 
 
-
-
-    // == success == //
-    public static <T> ApiResponse<T> success() {
-        return new ApiResponse<>(ResultCode.SUCCEES);
+    // == Error == //
+    /**
+     * 매개변수를 받지 않는 메서드
+     */
+    public static ApiResult error() {
+        return new ApiResult(ERROR);
     }
 
-    public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(ResultCode.SUCCEES, "message", data);
+    /**
+     * ResultCode를 매개변수로 받는 메서드
+     * 특정 Code에 대한 Response return
+     */
+    public static ApiResult error(ResultCode resultCode) {
+        return new ApiResult<>(resultCode);
     }
 
-    public static <T> ApiResponse<T> success(Integer code, String message) {
-        return new ApiResponse<>(code, message);
+    public static ApiResult error(int httpStatusCode, String message) {
+        return new ApiResult<>(httpStatusCode, message);
     }
 
-    public static <T> ApiResponse<T> success(Integer code, String message, T data) {
-        return new ApiResponse<>(code, message, data);
+    /**
+     * Resultcode, Data를 매개변수로 받는 메서드
+     */
+    public static <T> ApiResult<T> error(ResultCode resultCode, T data) {
+        return new ApiResult<>(resultCode, data);
     }
 
-    // == error == //
-    public static <T> ApiResponse<T> error() {
-        return new ApiResponse<>(ResultCode.ERROR);
+    /**
+     * 성공 리턴시 메시지 내용을 커스텀으로 작성
+     */
+    public static ApiResult error(String message) {
+        return new ApiResult(ERROR, message);
+    }
+
+
+    // == Valid err == //
+
+    /**
+     * @Valid 검증 오류 발생 시 첫번째 에러 메시지 리턴
+     */
+    public static ApiResult validErrorSingleRow(BindingResult bindingResult) {
+        String errmsg = bindingResult.getAllErrors().get(0).getDefaultMessage();
+        return new ApiResult(ResultCode.ERROR);
     }
 
 
 
-
-    // == binding result  == //
-    public static <T> ApiResponse<T> validError(BindingResult bindingResult) {
-        Map<String, String> errors = new HashMap<>();
-
-        List<ObjectError> allErrors = bindingResult.getAllErrors();
-        for (ObjectError error : allErrors) {
-            if (error instanceof FieldError) {
-                errors.put(((FieldError) error).getField(), error.getDefaultMessage());
-            } else {
-                errors.put( error.getObjectName(), error.getDefaultMessage());
-            }
-        }
-        return new ApiResponse(ResultCode.VALIDERR, errors);
-    }
-
-    public static <T> ApiResponse<T> validErrorSingle(BindingResult bindingResult) {
-        String firstMessage = bindingResult.getFieldErrors().get(0).getDefaultMessage();
-        return new ApiResponse(ResultCode.VALIDERR, firstMessage);
-    }
-
-    private ApiResponse(ResultCode code, T data) {
-        this.code = code.getCode();
-        this.message = code.getMessage();
-        this.data = data;
-    }
 }
