19cee8921b2a7da861ac28ac4c6edd587da57f12
song-eunji <eungjisong99@gmail.com>
2024-01-22 22:29:37 +0900

    feat(api response) : API 응답 코드 추가 및 응답 리턴 시 사용하지 않는 필드 제거

--- delivery-admin/src/main/java/com/example/deliveryadmin/common/response/ApiResult.java
+++ delivery-admin/src/main/java/com/example/deliveryadmin/common/response/ApiResult.java
@@ -13,35 +13,23 @@ import org.springframework.http.HttpStatus;
 public class ApiResult<T> {
     private final Integer code; // ResultCode.code
     private final String message; // ResultCode.message or custom message
-    private final HttpStatus httpStatus;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private final T data; // return data
-
-    public ApiResult(int httpStatusCode, String message) {
-        HttpStatus status = HttpStatus.valueOf(httpStatusCode);
-        this.code = httpStatusCode;
-        this.message = message;
-        this.httpStatus = status;
-        this.data = null;
-    }
 
     public ApiResult(ResultCode resultCode) {
         this.code = resultCode.getCode();
         this.message = resultCode.getMessage();
-        this.httpStatus = resultCode.getHttpStatus();
         this.data = null;
     }
 
     public ApiResult(ResultCode resultCode, T data) {
         this.code = resultCode.getCode();
         this.message = resultCode.getMessage();
-        this.httpStatus = resultCode.getHttpStatus();
         this.data = data;
     }
 
     public ApiResult(ResultCode resultCode, String message) {
         this.code = resultCode.getCode();
-        this.httpStatus = resultCode.getHttpStatus();
         this.message = message;
         this.data = null;
     }
