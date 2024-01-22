cef7deb2629ebb536207af707b7297cc65f1a34b
song-eunji <eungjisong99@gmail.com>
2024-01-22 22:30:42 +0900

    feat(security) : 입력받은 Token 값을 기준으로 사용자 id 추출 기능 추가

--- delivery-admin/src/main/java/com/example/deliveryadmin/common/util/SecurityUtil.java
+++ delivery-admin/src/main/java/com/example/deliveryadmin/common/util/SecurityUtil.java
@@ -1,9 +1,13 @@
 package com.example.deliveryadmin.common.util;
 
+import com.example.deliveryadmin.common.exception.NotFoundException;
+import com.example.deliveryadmin.common.response.ResultCode;
+import com.example.deliveryadmin.domain.member.Member;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
@@ -14,22 +18,26 @@ public class SecurityUtil {
 
    private SecurityUtil() {}
 
-   public static Optional<String> getCurrentUsername() {
+   /**
+    * 토큰을 이용한 사용자의 id값 리턴
+    */
+   public static Long getCurrentMemberId() {
       final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
       if (authentication == null) {
-         logger.debug("Security Context에 인증 정보가 없습니다.");
-         return Optional.empty();
+         throw new RuntimeException(" Security Context에 인증 정보가 없습니다.");
       }
 
-      String username = null;
+      Long adminId = null;
       if (authentication.getPrincipal() instanceof UserDetails) {
-         UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
-         username = springSecurityUser.getUsername();
-      } else if (authentication.getPrincipal() instanceof String) {
-         username = (String) authentication.getPrincipal();
+         Member springSecurityMember = (Member) authentication.getPrincipal();
+         adminId = springSecurityMember.getId();
       }
 
-      return Optional.ofNullable(username);
+      if (adminId == null) {
+         throw new RuntimeException("사용자 정보를 찾을 수 없습니다.");
+      }
+
+      return adminId;
    }
+
 }
