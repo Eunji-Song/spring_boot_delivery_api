84303f4ab5fffce761453a727279eb218a6b28e3
song-eunji <eungjisong99@gmail.com>
2024-01-25 02:56:13 +0900

    feat(fileupload) : multipart를 이용한 파일 업로드 기능 추가에 따른 응답코드 추가

---
+++ delivery-admin/src/main/java/com/example/deliveryadmin/common/fileupload/AttachmentFileMapper.java
@@ -0,0 +1,8 @@
+package com.example.deliveryadmin.common.fileupload;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface AttachmentFileMapper {
+    AttachmentFile dtoToEntity(AttachmentFileDto attachmentFileDto);
+}
