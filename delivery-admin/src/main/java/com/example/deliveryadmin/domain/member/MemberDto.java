c799e38d797a58ef388914ba9802c9faab7c3d08
song-eunji <eungjisong99@gmail.com>
2024-01-28 04:48:38 +0900

    refactor : 주석처리

--- delivery-admin/src/main/java/com/example/deliveryadmin/domain/member/MemberDto.java
+++ delivery-admin/src/main/java/com/example/deliveryadmin/domain/member/MemberDto.java
@@ -96,13 +96,17 @@ public class MemberDto {
             this.email = email;
         }
 
-        // Entity -> Dto
+        // 매개변수를 Member 엔티티로 받은 경우 DTO로 변환하여 리턴
         public DetailInfo(Member member) {
             this.id = member.getId();
             this.accountId = member.getAccountId();
             this.name = member.getName();
             this.email = member.getEmail();
         }
+
+
+
+
 
     }
 
